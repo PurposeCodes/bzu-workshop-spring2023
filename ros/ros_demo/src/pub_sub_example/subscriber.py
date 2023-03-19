d1067e8dce2828484248c8ade139564d903d54c3
meqdaddev <meqdad.darweesh@gmail.com>
2023-03-19 20:33:16 +0200

    add pub/sub ros example

---
+++ ros/ros_demo/src/pub_sub_example/subscriber.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+
+def purpose_callback(message):
+    #get_caller_id(): Get fully resolved name of local node
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", message.data)
+    
+def subscriber():
+    rospy.init_node('subscriber', anonymous=True)
+
+    rospy.Subscriber("purpose_speaker", String, purpose_callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    subscriber()
